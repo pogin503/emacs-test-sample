4d5523706bd1e9b8e2cb080f914b64cf493c5844
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2014-08-14 15:43:14 +0900

    Update myfile-test.el

--- test/myfile-test.el
+++ test/myfile-test.el
@@ -1,7 +1,13 @@
-;; test/myfile-test.el
+;;; ;; test/myfile-test.el --- myfile-test.el
+;; This program is free software
+;;; Commentary:
+;;; Code:
 (require 'ert)
-
 (require 'myfile)
 
 (ert-deftest myfile:addition-test ()
   (should (= (myfile:addition 1 2) 3)))
+
+(provide 'myfile-test)
+
+;;; myfile-test.el ends here
