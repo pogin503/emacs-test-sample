83f2e716974a508aff0431f560fa74e5a0909775
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2014-08-14 15:39:31 +0900

    Update myfile.el

--- myfile.el
+++ myfile.el
@@ -1,6 +1,11 @@
-;; myfile.el
+;;; myfile.el --- sample
+;; This program is free software
+;;; Commentary:
+;;; Code:
 
 (defun myfile:addition (a b)
   (+ a b))
 
 (provide 'myfile)
+
+;;; myfile.el ends here
